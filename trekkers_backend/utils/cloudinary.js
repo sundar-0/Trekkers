1c5ab18004b1ff58de8e7f072f3f7ddaa1cbfacb
Sundar Dumre <72253517+sundar-0@users.noreply.github.com>
2026-06-09 09:13:38 +0545

    Update cloudinary.js

--- trekkers_backend/utils/cloudinary.js
+++ trekkers_backend/utils/cloudinary.js
@@ -1,7 +1,1 @@
-const cloudinary=require('cloudinary')
-cloudinary.config({
-cloud_name:process.env.CLOUD_NAME||"trekkers",
-api_key:process.env.API_KEY||"163861366173299",
-api_secret:process.env.API_SECRET||"OD8IUxoVe1hp6dIhCnDHYQLwit4"
-}
-)
+
